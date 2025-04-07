8e30a0b9d18cbb9e7af50ac9de6719b88466a495
Christian Attorri <attorri@cua.edu>
2025-04-07 14:21:34 -0400

    bedrock with deepseek 🐋🐋

--- backend/app.py
+++ backend/app.py
@@ -1,46 +1,92 @@
-import sagemaker
-import boto3
-from sagemaker.huggingface import HuggingFace
+import boto3, json
 
-def train_model():
+bedrock = boto3.client(
+    service_name="bedrock",
+    region_name="us-east-1",
+)
+
+bedrock_runtime = boto3.client(
+    service_name="bedrock-runtime",
+    region_name="us-east-1",
+)
+
+"""
+DeepSeek R1 API Call
+    {
+  "modelId": "deepseek.r1-v1:0",
+  "contentType": "application/json",
+  "accept": "application/json",
+  "body": {
+    "inferenceConfig": {
+      "max_tokens": 512
+    },
+    "messages": [
+      {
+        "role": "user",
+        "content": "this is where you place your input text"
+      }
+    ]
+  }
+}
+"""
+
+model_id = 'deepseek.r1-v1:0'
+
+def model_summary(model_id):
+    response = bedrock.list_foundation_models()
+    for model in response['modelSummaries']:
+        if model['modelId'] == model_id:
+            return json.dumps(model, indent=0)
+    return None
+
+#print(model_summary(model_id))
+
+
+prompts = ["Explain traveling salesman problem for a 10 year old.",
+    "Explain why big 4 consulting firms want to replace their consultants with AI.",
+    "Explain the difference between a LLM and a chatbot." ,
+    "Explain why leetcode is a good way to prepare for technical interviews." ,
+    "Explain the bursting balloons leetcode problem to a child."
+    ]
+
+def converse_deepseek(prompt):
     
-    try:
-        role = sagemaker.get_execution_role()
-    except ValueError:
-        iam = boto3.client('iam')
-        role = iam.get_role(RoleName='sagemaker_execution_role')['Role']['Arn']
-        
-    bucket = 'sagemaker-us-east-1-123456789012' # Replace with your own bucket
-    hyperparams = {
-        'model_name_or_path': 'Qwen/Qwen2.5-Omni-7b',
-        'output_dir': 's3://{}/output'.format(bucket),
-        'max_seq_length': 1024,
-        'max_steps': 100,
-        'per_device_train_batch_size': 1,
-        'per_device_eval_batch_size': 1,
-        'learning_rate': 2e-5,
-        
-    }
+    system_messages = [
+        {
+            "text": "You are a helpful AI assistant",
+        }
+    ]
 
-    git_config = { 'repo': 'https://github.com/huggingface/transformers.git',
-                'branch': 'v4.37.0'
-    }
-
-    huggingface_estimator = HuggingFace(
-        entry_point='trainer.py',
-        source_dir='./trainer', # Path to the directory containing the trainer.py file
-        instance_type='ml.g5.2xlarge',
-        instance_count=1,
-        role=role,
-        git_config=git_config,
-        transformers_version='4.37.0',
-        hyperparameters=hyperparams,
-        pytorch_version='2.1.0',
-        py_version='py310',
+    messages = [
+        {
+           "role": "user",
+           "content": [
+               {
+                   "text": prompt,
+               }
+           ]
+        }
+    ]
+    
+    response = bedrock_runtime.converse(
+        modelId='us.deepseek.r1-v1:0',
+        inferenceConfig={
+            "maxTokens": 100,
+        },
+        system=system_messages,
+        messages=messages,
     )
-
-    huggingface_estimator.fit()
-
-if __name__ == "__main__":
-    train_model()
-
+    
+    return response
+    
+for prompt in prompts:
+    _json = converse_deepseek(prompt)
+    response = _json['output']
+    
+    # Print both reasoning and final answer
+    print("Prompt:", prompt)
+    print("\nReasoning:")
+    print(response[0]['text'])  # Reasoning
+    print("\nAnswer:")
+    print(response[1]['text'])  # Final answer
+    print("-"*100)
