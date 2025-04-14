1ea1f553a89456336e69795775836112475ee5fe
Christian Attorri <attorri@cua.edu>
2025-04-14 16:46:53 -0400

    begining of stability ai connection (img gen)

---
+++ ai/stability.py
@@ -0,0 +1,26 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv('STABILITY_API_KEY')
+
+response = requests.post(
+    f"https://api.stability.ai/v2beta/stable-image/generate/ultra",
+    headers={
+        "authorization": f"Bearer sk-MYAPIKEY",
+        "accept": "image/*"
+    },
+    files={"none": ''},
+    data={
+        "prompt": "Lighthouse on a cliff overlooking the ocean",
+        "output_format": "webp",
+    },
+)
+
+if response.status_code == 200:
+    with open("./lighthouse.webp", 'wb') as file:
+        file.write(response.content)
+else:
+    raise Exception(str(response.json()))
