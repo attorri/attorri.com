641f56b1c1b7ecbbcf95743b947e948c1fefd424
Christian Attorri <attorri@cua.edu>
2025-04-18 13:17:30 -0400

    updated yolo train - will need the cloud for this

--- server/train_yolo.py
+++ server/train_yolo.py
@@ -1,17 +1,19 @@
 from ultralytics import YOLO
+import os
 
-model = YOLO('yolov8n-cls.pt')
+model = YOLO('yolov8n-cls.pt') 
 
-data_dir = 'rock_paper_scissors'
-
-results= model.train(
-    data=data_dir,
-    epochs=32,
-    imgsz=300,
+# Train the model with 3 GPUs
+results = model.train(
+    data='rock_paper_scissors',
+    epochs=100,
+    imgsz=64,
     device='cpu'
 )
 
-last_run_dir = results.save_dir + '/weights/best.pt'
+last_run_dir = os.path.join(str(results.save_dir), 'weights', 'best.pt')
+
+model.export(format='onnx') 
 
 trained = YOLO(last_run_dir)
 
